5c738ec54ccdf1d60563d30b0855988b51b2a5d5
Naveen <adith.naveen@gmail.com>
2022-05-06 16:19:52 +0530

    updated with docker file snsc

--- spring-projects/spring-kafka-demo/src/main/java/com/ibm/kafka/spring/service/KafkaService.java
+++ spring-projects/spring-kafka-demo/src/main/java/com/ibm/kafka/spring/service/KafkaService.java
@@ -1,6 +1,7 @@
 package com.ibm.kafka.spring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,9 @@ public class KafkaService {
 	// when you say auto wired spring will do dependency injection 
 	@Autowired
 	private KafkaTemplate<String, String> kafkaTemplate; 
-	private String kafkaTopic = "spring-boot-topic"; 
+	// SpEL
+	@Value("${ibm.topic.name}")
+	private String kafkaTopic;
 	
 	// methods 
 	public void sendMessage(String message) {
